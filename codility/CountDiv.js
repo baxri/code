d8846914918ac344c0b073de88abfd82d6137ed2
giorgi <giorgi.bibilashvili89@gmail.com>
2018-06-23 09:05:54 +0400

    --

---
+++ codility/CountDiv.js
@@ -0,0 +1,30 @@
+// Solution Address
+// 
+
+let A = 4
+let B = 11
+// let B = 11
+let K = 3
+
+function solution(A, B, K) {
+
+    if(K > B){
+        return 0;
+    }
+
+    if(A == B && A%K == 0){
+        return 1;
+    }
+
+    let total = parseInt((B-A)/K);
+
+    if(A%K == 0){
+        total++;
+    }
+
+    return total;
+}
+
+let result = solution(A, B, K);
+
+console.log(result);

48ea5c7a883bd1bad3bdd4e08f6baff98db7ee13
giorgi <giorgi.bibilashvili89@gmail.com>
2018-06-25 21:26:20 +0400

    --

--- test.js
+++ test.js
@@ -1,44 +1,9 @@
 
-let A = [1, 1, 1, 1, 56,];
-
-// let N = [];
-
-// for (let i = 1; i <= 999999; i++) {
-//     N.push(i);
-
-//     if (i != 90) {
-//         N.push(i);
-//     }
-// }
 
 
-
-// console.log(N   );
-
-function solution(A) {
-    A.sort();
-    let i, j, count = 0;
-
-    for (i = 0; i < A.length; i++) {
-        for (j = i + 1; j < A.length; j++) {
-            if (A[i] == A[j])
-                count++;
-            else
-                break;
-        }
-
-        if (count % 2 == 0)
-            return A[i];
-        else {
-            count = 0;
-            i = j - 1;
-        }
-    }
-
-    return 0;
+let a = function (...args) {
+    console.log(args);
 }
 
 
-let result = solution(A);
-
-console.log(result);
+a(2, 6, 8, 9);

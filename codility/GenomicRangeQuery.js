f90d1ae1cb8811f4dd9ca117e3785790491f05f3
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2018-06-26 16:37:24 +0400

    --

--- codility/GenomicRangeQuery.js
+++ codility/GenomicRangeQuery.js
@@ -1,5 +1,5 @@
 // Solution Address
-// 
+// https://app.codility.com/demo/results/trainingA6WRUD-XUT/
 
 let S = "CAGCCTA";
 
@@ -8,22 +8,29 @@ let Q = [4, 5, 6];
 
 function solution(S, P, Q) {
 
-    for(var i=0; i<S.length; i++) {
-        console.log(value(S[i]));
+    S = S.split("");
+    for (var i = 0; i < S.length; i++) {
+        S[i] = value(S[i]);
     }
 
-   
-    console.log(S);
+    let result = [];
 
-    return S;
+    for (var i = 0; i < P.length; i++) {
+        let start = P[i];
+        let end = Q[i];
+        let range = S.slice(start, end + 1);
+        result.push(Math.min(...range));
+    }
+
+    return result;
 }
 
 function value(gene) {
-    if(gene == 'A') {
+    if (gene == 'A') {
         return 1;
-    } else if(gene == 'C') {
+    } else if (gene == 'C') {
         return 2;
-    } else if(gene == 'G') {
+    } else if (gene == 'G') {
         return 3;
     } else {
         return 4;

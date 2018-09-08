6c19e3c959017ed6af6e151cd0875af6e2a7375a
giorgi <giorgi.bibilashvili89@gmail.com>
2018-09-08 23:12:15 +0400

    --

--- cracking-interview/zero-matrix.js
+++ cracking-interview/zero-matrix.js
@@ -5,12 +5,28 @@ let matrix = [
     [1, 2, 3, 4, 5],
     [1, 2, 3, 0, 5],
     [1, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5],
 ];
 
 function zeroMatrix(matrix) {
 
-    console.log(matrix);
+    let rowsZero = [];
+    let columnsZero = [];
+
+    for (let row = 0; row < matrix.length; row++) {
+        for (let column = 0; column < matrix[row].length; column++) {
+
+            if (matrix[row][column] == 0) {
+
+                rowsZero.push(row);
+                columnsZero.push(column);
+
+                
+
+            }
+
+        }
+    }
+
 
 }
 

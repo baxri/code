24b8a5da54f690e910bc63b36f28c2ca633011fa
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2018-09-10 11:47:55 +0400

    --

--- cracking-interview/rotate-matrix-1.js
+++ cracking-interview/rotate-matrix-1.js
@@ -1,41 +1,36 @@
 
 let matrix = [
-    [1, 2, 3],
-    [1, 2, 3],
-    [1, 2, 3],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
 ];
 
 function rotateMatrinx(matrix) {
 
-    for (let level = 0; level < matrix.length / 2; level++) {
-        let first = level;
+    for (let level = 0; level < 2; level++) {
+
         let last = matrix.length - 1 - level;
 
-        for (let i = first; i < last; i++) {
-            let offset = i - first;
-
-            console.log(first);
-            console.log(i);
-
-            console.log(last - offset);
-            console.log(first);
-
-
-
-            let top = matrix[first][i];
-
-            matrix[first][i] = matrix[last - offset][first];
-
-
-
-            console.log(top);
+        for (let first = level; first < last; first++) {
+            matrix = swap(matrix, level, first, first, last);
+            matrix = swap(matrix, level, first, last, last - first);
+            matrix = swap(matrix, level, first, last - first, level);
         }
 
     }
 
+    console.log(matrix);
 
 }
 
+function swap(matrix, a, b, c, d) {
 
+    let temp = matrix[a][b];
+    matrix[a][b] = matrix[c][d];
+    matrix[c][d] = temp;
+    return matrix;
+}
 
 console.log(rotateMatrinx(matrix));

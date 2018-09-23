fd94d676ba367550e43714a335a8a3485c9fd122
giorgi <giorgi.bibilashvili89@gmail.com>
2018-09-23 20:43:25 +0400

    --

---
+++ cracking-interview/Linked LIsts/stack.js
@@ -0,0 +1,68 @@
+
+
+class Stack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(item) {
+
+        if (this.minStack.length == 0 || item <= this.minStack[0]) {
+            this.minStack = [item, ...this.minStack];
+        }
+
+        this.stack = [item, ...this.stack];
+    }
+
+    pop() {
+
+        if (this.minStack[0] == this.stack[0]) {
+            this.minStack.shift();
+        }
+
+        this.stack.shift();
+    }
+
+    pick() {
+        return this.stack[0];
+    }
+
+    isEmpty() {
+        return this.stack.length == 0
+    }
+
+    min() {
+        return this.minStack[0];
+    }
+
+    get() {
+        return this.stack;
+    }
+}
+
+let stack = new Stack();
+
+stack.push(4);
+stack.push(3);
+stack.push(5);
+stack.push(3);
+stack.push(3);
+
+
+stack.pop();
+
+
+console.log(stack.min());
+
+console.log(stack.isEmpty());
+
+console.log(stack.pick());
+
+console.log(stack.get());
+
+
+
+console.log(stack);
+
+

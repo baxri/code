b3aea081ea5ec082052549d63a25b8e159467c07
giorgi <giorgi.bibilashvili89@gmail.com>
2018-10-07 19:17:48 +0400

    --

--- cracking-interview/Trees and Graphs/minimal-tree.js
+++ cracking-interview/Trees and Graphs/minimal-tree.js
@@ -1,10 +1,43 @@
 
 
-let array = [1, 2, 3, 4];
+let array = [1, 2, 3, 4, 5, 6, 7];
 
 class Tree {
-    constructor() {
-        this.root = null;
+    constructor(array) {
+        this.root = this.buildFromArray(array);
+    }
+
+    buildFromArray(array) {
+
+        let start = 0;
+        let end = array.length - 1;
+
+        if (array.length < 1) {
+            return null;
+        }
+
+        let middle = parseInt((start + end) / 2);
+
+        let left = array.slice(0, middle);
+        let right = array.slice(middle + 1);
+
+        let leftLeaf = this.buildFromArray(left);
+
+        let node = new Node(array[middle]);
+
+        if (leftLeaf) {
+            node.addChild(leftLeaf);
+        }
+
+        // console.log(array[middle]);
+
+        let rightLeaf = this.buildFromArray(right);
+
+        if (rightLeaf) {
+            node.addChild(rightLeaf);
+        }
+
+        return node;
     }
 }
 
@@ -19,47 +52,10 @@ class Node {
     }
 }
 
-function minimalTree(array, tree) {
 
-    let start = 0;
-    let end = array.length - 1;
 
-    if (array.length < 1) {
-        return null;
-    }
+let tree = new Tree(array);
 
-    let middle = parseInt((start + end) / 2);
-
-    let left = array.slice(0, middle);
-    let right = array.slice(middle + 1);
-
-    // console.log(middle);
-    // console.log(left, );
-    // console.log(right);
-
-    // if (array.length > 1) {
-    let leftLeaf = minimalTree(left, tree);
-    // }
-
-    let node = new Node(array[middle]);
-
-    if (leftLeaf) {
-        node.addChild(leftLeaf);
-    }
-
-    console.log(array[middle]);
-
-    let rightLeaf = minimalTree(right, tree);
-
-    if (rightLeaf) {
-        node.addChild(rightLeaf);
-    }
-
-    return node;
-}
-
-let tree = new Tree();
-
-console.log(minimalTree(array, tree));
+console.log(tree);
 
 

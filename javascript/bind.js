c5d0d2bde16440d7b3e0a5b47f3e36630901384a
giorgi <giorgi.bibilashvili89@gmail.com>
2018-11-03 13:06:06 +0400

    Examples of this and bind - Object Creation in JavaScript P2 - FunFunFunction #44

--- javascript/bind.js
+++ javascript/bind.js
@@ -1,6 +1,42 @@
+// Inspiration video
+// Fun fun function
+// https://www.youtube.com/watch?v=GhbhD1HR5vk
+// https://www.youtube.com/watch?v=PIkA60I0dKU
 
 
+let dog = {
+    sound: "woof",
+    talk: function () {
+        console.log(this.sound);
+    }
+}
+
+dog.talk();
+
+let talkFunction = dog.talk; // woof
+
+// Lets bind this function to dog object
+let boundFunction = talkFunction.bind(dog);
+
+talkFunction(); // undefined
+boundFunction(); // woof
 
 
+function talk() {
+    console.log(this.sound);
+}
 
-console.log("OKOKOK");
+let boromir = {
+    blaber: talk,
+    sound: 'On does not simply walk into mordor',
+};
+
+let gollum = {
+    jabber: boromir.blaber,
+    sound: 'My precioussssss...',
+}
+
+gollum.jabber()
+
+boromir.blaber();
+

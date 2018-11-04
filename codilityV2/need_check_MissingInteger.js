a8c2f0d446b3a86319727eb5de6cc1bb3afcc4ce
giorgi <giorgi.bibilashvili89@gmail.com>
2018-11-04 19:12:35 +0400

    --

---
+++ codilityV2/need_check_MissingInteger.js
@@ -0,0 +1,35 @@
+
+let A = [1];
+
+
+function solution(A) {
+
+    A = A.sort();
+
+    let missing = 1;
+
+    for (let i = 0; i < A.length; i++) {
+
+        let current = A[i];
+
+        if (current <= 0) {
+            continue;
+        }
+
+        if (current > missing) {
+            return missing;
+        }
+
+        let next = A[i + 1];
+
+        if (next != current) {
+            missing++;
+        }
+
+    }
+
+    return missing;
+}
+
+
+console.log(solution(A));

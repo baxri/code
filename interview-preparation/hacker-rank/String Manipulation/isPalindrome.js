a98427e479fead3481c21f4b8fd968e2222ea72d
giorgi <giorgi.bibilashvili89@gmail.com>
2019-01-22 22:17:42 +0400

    --

---
+++ interview-preparation/hacker-rank/String Manipulation/isPalindrome.js
@@ -0,0 +1,21 @@
+
+
+function isPalindrome(word) {
+
+    word = word.toLowerCase();
+    word = word.split('');
+
+    for (let i = 0; i < word.length / 2; i++) {
+
+        let left = word[i];
+        let right = word[word.length - i - 1];
+
+        if (left != right) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPalindrome('Deleveled'));

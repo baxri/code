4265a7fa9d222db75b64ebd43292155d3ede4edf
giorgi <giorgi.bibilashvili89@gmail.com>
2019-09-17 00:52:13 +0400

    --

--- 2019/hackerrank/interview-preparation-kit/dictionaries-hash/count-triplets.js
+++ 2019/hackerrank/interview-preparation-kit/dictionaries-hash/count-triplets.js
@@ -1,9 +1,24 @@
-
-
-
 let a = 3;
-console.log('a', a)
+console.log("a", a);
 
 function countTriplets(arr, r) {
+  let counter = 0;
 
+  const combinations = {};
+  const posibilities = {};
+
+  arr.forEach(numner => {
+    counter += combinations[numner] || 0;
+
+    const nextNumber = numner * r;
+
+    combinations[nextNumber] =
+      (combinations[nextNumber] || 0) + (posibilities[numner] || 0);
+
+    posibilities[nextNumber] = (posibilities[nextNumber] || 0) + 1;
+  });
+
+  return counter;
 }
+
+console.log(countTriplets([1, 5, 5, 25, 125], 5));

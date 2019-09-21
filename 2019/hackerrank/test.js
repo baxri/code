b27b69defc155e496787a46e23c3b02c8eb18273
giorgi <giorgi.bibilashvili89@gmail.com>
2019-09-21 17:19:44 +0400

    --

---
+++ 2019/hackerrank/test.js
@@ -0,0 +1,22 @@
+function formatNumber(number) {
+  if (number == 0) {
+    return "0.00";
+  }
+
+  number = parseInt(number * 100) / 100 + "";
+
+  number = number.split(".");
+
+  const first = number[0];
+  let second = number[1] || [];
+
+  if ((second.length = 0)) {
+    second = "00";
+  } else if (second.length == 1) {
+    second = second + "0";
+  }
+
+  return first + "." + second;
+}
+
+console.log(formatNumber(0));

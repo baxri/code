bdadd967b61f5e328fdc565e9388c84bbbc45e7d
giorgi <giorgi.bibilashvili89@gmail.com>
2019-09-24 21:57:12 +0400

    --

--- 2019/hackerrank/interview-preparation-kit/string-manipulation/makeing-anagrams.js
+++ 2019/hackerrank/interview-preparation-kit/string-manipulation/makeing-anagrams.js
@@ -3,28 +3,31 @@ function makeAnagram(a, b) {
   let bHash = {};
 
   for (let i = 0; i < a.length; i++) {
-    aHash[a[i]] = aHash[a[i]] + 1 || 1;
+    aHash[a[i]] = aHash[a[i]] ? aHash[a[i]] + 1 : 1;
   }
 
-  for (let i = 0; i < a.length; i++) {
-    bHash[a[i]] = bHash[b[i]] + 1 || 1;
+  for (let i = 0; i < b.length; i++) {
+    bHash[b[i]] = bHash[b[i]] ? bHash[b[i]] + 1 : 1;
   }
 
   let removeCount = 0;
 
   Object.keys(aHash).map(key => {
-    if (bHash[key]) {
-    //   removeCount = Math.abs(aHash[key], bHash[key]);
-    }
+    removeCount += Math.abs(aHash[key] - (bHash[key] || 0));
 
-    // delete(aHash[key])
-    console.log(key)
-    // delete(bHash[key])
+    delete bHash[key];
   });
 
+  Object.keys(bHash).map(key => {
+    removeCount += bHash[key];
+  });
+
+  console.log(removeCount);
   console.log(aHash);
   console.log(bHash);
+
+  return removeCount;
 }
 
-// console.log(makeAnagram("cde", "dcf"));
+console.log(makeAnagram("cde", "dcf"));
 console.log(makeAnagram("cde", "abc"));

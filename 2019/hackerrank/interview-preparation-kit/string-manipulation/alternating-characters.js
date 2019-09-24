d42ef8666fb4d84ba49f5d003ca7eeff9ae3db27
giorgi <giorgi.bibilashvili89@gmail.com>
2019-09-24 23:11:57 +0400

    --

--- 2019/hackerrank/interview-preparation-kit/string-manipulation/alternating-characters.js
+++ 2019/hackerrank/interview-preparation-kit/string-manipulation/alternating-characters.js
@@ -2,7 +2,7 @@ function alternatingCharacters(s) {
   let lastElement = null;
   let deleteCount = 0;
 
-  s.split("").forEach((char, key) => {
+  s.split("").forEach(char => {
     if (lastElement == char) {
       deleteCount++;
     } else {

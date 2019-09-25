d2ce5e26f802dab6d154da5da31366bfee1ad812
giorgi <giorgi.bibilashvili89@gmail.com>
2019-09-25 14:34:47 +0400

    --

--- 2019/hackerrank/interview-preparation-kit/string-manipulation/common-child.js
+++ 2019/hackerrank/interview-preparation-kit/string-manipulation/common-child.js
@@ -1,51 +1,47 @@
 function commonChild(s1, s2) {
+  const matrix = buildMatrix(s1, s2);
+
+    
+
+  return matrix[s2.length][s1.length];
+}
+
+function buildMatrix(s1, s2) {
   s1 = s1.split("");
   s2 = s2.split("");
 
-  const matrix = new Array(7).fill(0).map(() => new Array(6).fill(0));
+  const matrix = new Array(s2.length + 1)
+    .fill(0)
+    .map(() => new Array(s1.length + 1).fill(0));
 
-  for (let i = 0; i < s1.length + 2; i++) {
-    for (let j = 0; j < s1.length + 2; j++) {
-      if (i < 2 && j < 2) {
-        matrix[i][j] = 0;
-      }
+  for (let i = 1; i <= s1.length; i++) {
+    for (let j = 1; j <= s2.length; j++) {
+      let lastS1 = s1[j - 1];
+      let lastS2 = s2[i - 1];
 
-      if (i == 0 && j > 1) {
-        matrix[i][j] = s1[j - 2];
-      }
+      let left = matrix[i][j - 1];
+      let top = matrix[i - 1][j];
+      let diagonal = matrix[i - 1][j - 1];
 
-      if (i == 1 && j > 1) {
-        matrix[i][j] = 0;
-      }
-
-      if (j == 0 && i > 1) {
-        matrix[i][j] = s2[i - 2];
-      }
-
-      if (j == 1 && i > 1) {
-        matrix[i][j] = 0;
+      if (lastS1 == lastS2) {
+        str += lastS1;
+        matrix[i][j] = diagonal + 1;
+      } else {
+        matrix[i][j] = Math.max(left, top);
       }
     }
   }
 
-  for (let i = 2; i < s1.length + 2; i++) {
-    for (let j = 2; j < s1.length + 2; j++) {
-        
-        let last_s1 = s1[j-2]
-        let last_s2 = s1[i-2]
-        
-        if(  last_s1 == last_s2 ){
-            matrix[i][j] = matrix[i-1][j-1] + 1
-        }else{
-            matrix[i][j] = Math.max(matrix[i-1][j], matrix[i][j-1])
-        }
-    } 
-  }
-
-  console.log(matrix)
-
-  return matrix[matrix.length-1][matrix.length-1]
+  return matrix;
 }
+
+//   A B C D
+// A 1 1 1 1
+// B 1 2 2 2
+// D 1 2 2 3
+// C 1 2 3 3
+
+console.log(commonChild("ABCD", "ABDC"));
 
 // function commonChild(s1, s2) {
 //   return commonChildRec(s1, s2, s1.length - 1, s2.length - 1);
@@ -65,5 +61,3 @@ function commonChild(s1, s2) {
 //     commonChildRec(s1, s2, s1i, s2i - 1)
 //   );
 // }
-
-console.log(commonChild("AGGTAB", "GXTXAYB"));

739fc050783a9bbf93241717bf5b130d0d7a080e
giorgi <giorgi.bibilashvili89@gmail.com>
2019-09-27 02:09:47 +0400

    --

---
+++ 2019/hackerrank/interview-preparation-kit/arrays/array-left-rotation.js
@@ -0,0 +1,21 @@
+function rotLeft(a, d) {
+  let rotatedArray = new Array(a.length);
+
+  a.forEach((element, i) => {
+    if (d > a.length) {
+      d = d % a.length;
+    }
+
+    let newIndex = i - d;
+
+    if (newIndex < 0) {
+      newIndex = a.length + newIndex;
+    }
+
+    rotatedArray[newIndex] = element;
+  });
+
+  return rotatedArray;
+}
+
+console.log(rotLeft([1, 2, 3, 4, 5], 6));

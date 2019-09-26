a9531ed0cfca426cf331f48f92df3ea4715bfeec
giorgi <giorgi.bibilashvili89@gmail.com>
2019-09-26 11:47:17 +0400

    --

--- 2019/hackerrank/interview-preparation-kit/string-manipulation/shelok-and-valid-string.js
+++ 2019/hackerrank/interview-preparation-kit/string-manipulation/shelok-and-valid-string.js
@@ -1,10 +1,44 @@
-
-
-
 function isValid(s) {
+  let hash = {};
 
+  for (let i = 0; i < s.length; i++) {
+    hash[s[i]] = hash[s[i]] + 1 || 1;
+  }
 
+  let lastCheckedElement = null;
+  let isValid = true;
+  let canRemove = true;
+
+  let ocurencies = [];
+
+  Object.keys(hash).map(key => {
+    ocurencies.push(hash[key]);
+  });
+
+  ocurencies.sort();
+
+  ocurencies.forEach(number => {
+    if (lastCheckedElement !== null) {
+      if (lastCheckedElement !== number) {
+        if (!canRemove) {
+          isValid = false;
+        } else {
+          if (Math.abs(lastCheckedElement - number) === 1) {
+            canRemove = false;
+            number--;
+          } else {
+            isValid = false;
+          }
+        }
+      }
+    }
+
+    lastCheckedElement = number;
+  });
+
+  console.log('hash', hash)
+
+  return isValid ? "YES" : "NO";
 }
 
-
-console.log(isValid(''))
+console.log(isValid("abcdefghhgfedecba"));

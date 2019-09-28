b427684fea2fc599ad2a303a488c67c25c2f19dc
giorgi <giorgi.bibilashvili89@gmail.com>
2019-09-28 13:18:02 +0400

    --

---
+++ 2019/hackerrank/interview-preparation-kit/arrays/array-manipulation.js
@@ -0,0 +1,51 @@
+function arrayManipulation(n, queries) {
+  // array of zeros of 10
+  let maxValue = 0;
+
+  let finalOperations = {};
+
+  for (let i = 0; i < queries.length; i++) {
+    let left = queries[i][0] - 1;
+    let right = queries[i][1] - 1;
+    let value = queries[i][2];
+
+    for (j = left; j <= right; j++) {
+      finalOperations[j] =
+        typeof finalOperations[j] !== "undefined"
+          ? finalOperations[j] + value
+          : value;
+
+      if (maxValue < finalOperations[j]) {
+        maxValue = finalOperations[j];
+      }
+    }
+  }
+
+  return maxValue;
+}
+
+function arrayManipulationV1(n, queries) {
+  // array of zeros of 10
+  let zeros = new Array(n).fill(0);
+  let maxValue = 0;
+
+  for (let i = 0; i < queries.length; i++) {
+    let left = queries[i][0] - 1;
+    let right = queries[i][1] - 1;
+    let value = queries[i][2];
+
+    for (let j = 0; j < zeros.length; j++) {
+      if (j >= left && j <= right) {
+        zeros[j] += value;
+
+        if (zeros[j] > maxValue) {
+          maxValue = zeros[j];
+        }
+      }
+    }
+  }
+
+  return maxValue;
+}
+
+console.log(arrayManipulation(10, [[1, 5, 3], [4, 8, 7], [6, 9, 1]]));

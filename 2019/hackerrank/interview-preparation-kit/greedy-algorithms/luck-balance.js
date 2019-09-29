e13abe363f8f2d1b82768886834a9577efc32c45
giorgi <giorgi.bibilashvili89@gmail.com>
2019-09-29 22:27:12 +0400

    --

--- 2019/hackerrank/interview-preparation-kit/greedy-algorithms/luck-balance.js
+++ 2019/hackerrank/interview-preparation-kit/greedy-algorithms/luck-balance.js
@@ -1,19 +1,29 @@
-
-
-
-
-
 function luckBalance(k, contests) {
+  contests.sort((a, b) => b[0] - a[0]);
 
-    console.log('contests', contests)
+  let sum = 0;
 
+  for (let i = 0; i < contests.length; i++) {
+    let value = contests[i][0];
+    let importance = contests[i][1];
+
+    if (importance === 1) {
+      if (k > 0) {
+        sum += value;
+        k--;
+      } else {
+        sum -= value;
+      }
+    } else {
+      sum += value;
+    }
+  }
+
+  return sum;
 }
 
+console.log(luckBalance(1, [[5, 1], [1, 1], [4, 0]]));
 
-console.log(1, luckBalance(
-    [
-        [5, 1],
-        [1, 1],
-        [4, 0],
-    ]
-))
+// 5, 1;
+// 4, 0;
+// 1, 1;

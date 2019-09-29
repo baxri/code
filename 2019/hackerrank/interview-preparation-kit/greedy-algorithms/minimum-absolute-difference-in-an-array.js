3b664c7cf429ca49f3687901fdee9185b8e2f04f
giorgi <giorgi.bibilashvili89@gmail.com>
2019-09-29 22:43:26 +0400

    --

---
+++ 2019/hackerrank/interview-preparation-kit/greedy-algorithms/minimum-absolute-difference-in-an-array.js
@@ -0,0 +1,16 @@
+function minimumAbsoluteDifference(arr) {
+  let min = null;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let diference = Math.abs(arr[i] - arr[j]);
+
+      if (diference < min || min === null) {
+        min = diference;
+      }
+    }
+  }
+  return min
+}
+
+console.log(minimumAbsoluteDifference([-2, 2, 4]));

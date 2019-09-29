0bdd00f7171327b105d1bb33a5821d572f522089
giorgi <giorgi.bibilashvili89@gmail.com>
2019-09-29 22:18:09 +0400

    --

---
+++ 2019/hackerrank/interview-preparation-kit/greedy-algorithms/luck-balance.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+function luckBalance(k, contests) {
+
+    console.log('contests', contests)
+
+}
+
+
+console.log(1, luckBalance(
+    [
+        [5, 1],
+        [1, 1],
+        [4, 0],
+    ]
+))

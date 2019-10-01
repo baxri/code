54feae7550af275d172d3b72feb05923c4634a11
giorgi <giorgi.bibilashvili89@gmail.com>
2019-10-01 15:02:35 +0400

    --

---
+++ 2019/hackerrank/interview-preparation-kit/greedy-algorithms/reverse-shuffle-merge.js
@@ -0,0 +1,24 @@
+// Complete the reverseShuffleMerge function below.
+function reverseShuffleMerge(s) {
+  s = s.split("");
+
+  let splitIndex = s.length / 2;
+
+  let left = s.slice(0, splitIndex).reverse();
+
+  let i = 0;
+
+  while (left.length > 0) {
+    if (s[i] == left[0]) {
+      left.splice(0, 1);
+      s.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+
+  return s.join("");
+}
+
+console.log(reverseShuffleMerge("eggegg"));
+// console.log(reverseShuffleMerge("abcdefgabcdefg"));

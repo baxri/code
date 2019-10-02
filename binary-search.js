c832c9d6f0d7654ce752ef3c1ee20f62d213f0d8
giorgi <giorgi.bibilashvili89@gmail.com>
2019-10-02 13:59:19 +0400

    --

--- binary-search.js
+++ binary-search.js
@@ -16,4 +16,4 @@ function binarySearch(arr, search, left = 0, right = arr.length - 1) {
   }
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));

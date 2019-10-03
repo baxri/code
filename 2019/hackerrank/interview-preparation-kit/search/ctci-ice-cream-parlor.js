35bdbcc42fcd13116b547435df6c71ba093fe213
giorgi <giorgi.bibilashvili89@gmail.com>
2019-10-03 05:52:30 +0400

    --

--- 2019/hackerrank/interview-preparation-kit/search/ctci-ice-cream-parlor.js
+++ 2019/hackerrank/interview-preparation-kit/search/ctci-ice-cream-parlor.js
@@ -1,29 +1,36 @@
 // Complete the whatFlavors function below.
 
 function whatFlavors(cost, money) {
-  let hash = {};
-
-
-  console.log('cost', cost.slice())
-
+  
   let sortedMenu = [...cost].sort((a, b) => a - b);
 
-  for (let i = 0; i < cost.length; i++) {
-    if (typeof hash[cost[i]] === "undefined") {
-      hash[cost[i]] = i + 1;
-    }
-  }
-
   for (let i = 0; i < sortedMenu.length; i++) {
-    let search = money - cost[i];
+    let complement = money - sortedMenu[i];
 
-    let comp = cost.slice(i + 1).indexOf(search)
+    let location = recursiveFunction(sortedMenu, complement, i + 1)
 
-    if (comp > -1) {
-      console.log(i + 1, comp + 1 + i + 1);
+
+    if (location !== null && complement === sortedMenu[location]) {
+      console.log(getIndexes(cost, sortedMenu[i], complement).join(' ')) 
       break;
     }
   }
+}
+
+function getIndexes(menu, value1, value2){
+  let index1 = indexOf(menu, value1, -1)
+  let index2 = indexOf(menu, value2, index1)
+
+  return [Math.min(index1, index2) + 1, Math.max(index1, index2) + 1]
+}
+
+function indexOf(menu, value, excludeThis){
+  for (let i = 0; i < menu.length; i++) {
+    if(menu[i] === value && i !== excludeThis){
+      return i
+    }
+  }
+  return -1
 }
 
 function binarySearch(arr, search, left = 0, right = arr.length - 1) {
@@ -31,10 +38,10 @@ function binarySearch(arr, search, left = 0, right = arr.length - 1) {
     return null;
   }
 
-  let mid = Math.floor((left + right) / 2);
+  let mid=Math.floor((left + right)/2); 
 
   if (arr[mid] === search) {
-    return arr[mid];
+    return mid;
   } else if (search > arr[mid]) {
     return binarySearch(arr, search, mid + 1, right);
   } else if (search < arr[mid]) {
@@ -42,5 +49,28 @@ function binarySearch(arr, search, left = 0, right = arr.length - 1) {
   }
 }
 
+let recursiveFunction = function (arr, x, start, end) { 
+       
+  // Base Condtion 
+  if (start > end) return false; 
+ 
+  // Find the middle index 
+  let mid=Math.floor((start + end)/2); 
+ 
+  // Compare mid with given key x 
+  if (arr[mid]===x) return mid; 
+        
+  // If element at mid is greater than x, 
+  // search in the left half of mid 
+  if(arr[mid] > x)  
+      return recursiveFunction(arr, x, start, mid-1); 
+  else
+
+      // If element at mid is smaller than x, 
+      // search in the right half of mid 
+      return recursiveFunction(arr, x, mid+1, end); 
+} 
+
 console.log(whatFlavors([1, 4, 5, 3, 2], 4));
 // console.log(whatFlavors([2, 2, 4, 3], 4));
+

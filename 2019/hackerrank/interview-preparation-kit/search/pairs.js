b88f9fca46adefe483c10b13c7d472a46e8687e2
giorgi <giorgi.bibilashvili89@gmail.com>
2019-10-10 22:24:15 +0400

    --

--- 2019/hackerrank/interview-preparation-kit/search/pairs.js
+++ 2019/hackerrank/interview-preparation-kit/search/pairs.js
@@ -23,7 +23,6 @@ function binarySearch(arr, search, left = 0, right = arr.length - 1) {
   }
 
   let mid = Math.floor((left + right) / 2);
-
   if (arr[mid] === search) {
     return arr[mid];
   } else if (search > arr[mid]) {

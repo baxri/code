e270c34eb42f0df030d68ff0e991b0eace74b20c
giorgi <giorgi.bibilashvili89@gmail.com>
2019-10-12 13:54:21 +0400

    --

--- 2019/hackerrank/interview-preparation-kit/search/triple-sum.js
+++ 2019/hackerrank/interview-preparation-kit/search/triple-sum.js
@@ -1,11 +1,19 @@
-
-
-
 function triplets(a, b, c) {
+  a.sort((a, b) => a - b);
+  b.sort((a, b) => a - b);
+  c.sort((a, b) => a - b);
 
-    
+  console.log(a, b, c);
 
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < b.length; j++) {
+      for (let k = 0; k < c.length; k++) {
+        if (a[i] < b[j] && b[j] < c[j]) {
+          console.log([a[i], b[j], c[j]]);
+        }
+      }
+    }
+  }
 }
 
-
-console.log(triplets([3, 5, 7], [3, 6], [4, 6, 9]))
+console.log(triplets([3, 5, 7], [3, 6], [4, 6, 9]));
